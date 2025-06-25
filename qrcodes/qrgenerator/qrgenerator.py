739eb1e093625e23bd5a3fc64f71e9d802f2ceef
k0styanich <144476242+k0styanich@users.noreply.github.com>
2025-06-25 17:24:37 +0300

    Delete qrcodes/qrgenerator directory

--- qrcodes/qrgenerator/qrgenerator.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-
-content = input("Type your content for your qr code: ")
-name = input("Now type a name for your file: ")
-
-img = qrcode.make(content)
-img.save(f"qrcodes/{name}.png")
-print(f"Done! Your qr code is saved as {name}.png")
